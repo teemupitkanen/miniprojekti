38ea8e4aa6d4843b7a0876cf05dab6411b664a76
41407 <jimi.welling@cs.helsinki.fi>
2014-04-01 10:49:32 +0300

    Add Inproceedings

---
+++ miniprojektiv2/src/main/java/com/ohtu/miniprojektiv2/domain/Inproceedings.java
@@ -0,0 +1,54 @@
+package com.ohtu.miniprojektiv2.domain;
+
+import org.hibernate.validator.constraints.NotBlank;
+
+public class Inproceedings {
+
+    @NotBlank
+    private String author;
+    @NotBlank
+    private String title;
+    @NotBlank
+    private String booktitle;
+    @NotBlank
+    private String year;
+
+    public Inproceedings(String author, String title, String booktitle, String year) {
+        this.author = author;
+        this.title = title;
+        this.booktitle = booktitle;
+        this.year = year;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getBooktitle() {
+        return booktitle;
+    }
+
+    public void setBooktitle(String booktitle) {
+        this.booktitle = booktitle;
+    }
+
+    public String getYear() {
+        return year;
+    }
+
+    public void setYear(String year) {
+        this.year = year;
+    }
+}
