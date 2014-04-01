1692605d5786ff476d754e96e9bc07690608acc1
Viivi <viivi@viivi-1215B-1215B.(none)>
2014-04-01 11:06:16 +0300

    CitationService added

---
+++ miniprojektiv2/src/main/java/com/ohtu/miniprojektiv2/controller/InMemoryCitationService.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.ohtu.miniprojektiv2.controller;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class InMemoryCitationService implements CitationService{
+   
+   /**
+    *
+    */
+   public InMemoryCitationService(){
+      
+   }
+   
+}
