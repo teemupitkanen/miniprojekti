da5167859a41aedbd328c3388113000ab43b56dc
Lauri Vuojola <lauri.vuojola@helsinki.fi>
2014-04-07 12:25:55 +0300

    controller test

--- miniprojektiv2/src/test/java/com/ohtu/miniprojektiv2/controller/HomeControllerTest.java
+++ miniprojektiv2/src/test/java/com/ohtu/miniprojektiv2/controller/HomeControllerTest.java
@@ -1,30 +1,43 @@
 
 package com.ohtu.miniprojektiv2.controller;
 
-//import org.junit.springframework.test.web.server.MockMvc;
-//import org.junit.AfterClass;
-//import org.junit.BeforeClass;
-//import org.junit.Test;
+import org.junit.AfterClass;
+import static org.junit.Assert.assertTrue;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.openqa.selenium.*;
+import org.openqa.selenium.htmlunit.HtmlUnitDriver;
 
 /**
  * Tests for HomeController.
  * 
  */
 public class HomeControllerTest {
+    WebDriver driver;
     
+    @Before
+    public void setUp() {
+        driver = new HtmlUnitDriver();
+    }
     
-//    @BeforeClass
-//    public void setUp() {
-//        // code that will be invoked before this test starts
-//    }
-//    
-//    @Test
-//    public void aTest() {
-//        System.out.println("Test");
-//    }
-//    
-//    @AfterClass
-//    public void cleanUp() {
-//        // code that will be invoked after this test ends
-//    }
+    @Test
+    public void viewNewIsShownWhenLinkIsClicked() throws InterruptedException {
+        driver.get("http://localhost:8090/new");
+        WebElement element = driver.findElement(By.name("author"));
+        element.sendKeys("erkki esimerkki");
+
+        element = driver.findElement(By.name("title"));
+        element.sendKeys("foo");
+
+        element = driver.findElement(By.name("booktitle"));
+        element.sendKeys("bar");
+
+        element = driver.findElement(By.name("year"));
+        element.sendKeys("2014");
+
+        element.submit();
+        assertTrue(driver.getPageSource().contains("foo"));
+    }
+   
 }
