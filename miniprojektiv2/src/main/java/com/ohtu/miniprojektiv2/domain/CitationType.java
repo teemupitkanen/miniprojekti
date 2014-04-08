1b30b573abf7ca325fb5304581cb1ce3e2d3e39e
Viivi <viivi@viivi-1215B-1215B.(none)>
2014-04-08 13:09:35 +0300

    Citation and tests fixed

--- miniprojektiv2/src/main/java/com/ohtu/miniprojektiv2/domain/CitationType.java
+++ miniprojektiv2/src/main/java/com/ohtu/miniprojektiv2/domain/CitationType.java
@@ -3,7 +3,6 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package com.ohtu.miniprojektiv2.domain;
 
 /**
@@ -11,20 +10,22 @@ package com.ohtu.miniprojektiv2.domain;
  * @author 41407
  */
 public enum CitationType {
-    INPROCEEDINGS(new String[] {"author", "title", "booktitle", "year", // <- mandatory fields
-                "editor", "volumeNumber", "series", "pages", "address",
-                "month", "organization", "publisher", "note", "key"}),
-    BOOK(new String[] {"jotain"}); //JNE
 
-    private final String[] applicableFields;
-//    private final String citeType;
-    private CitationType(String[] applicableFields){
-//       this.citeType = citeType;
-        this. applicableFields = applicableFields;
-    }
+   INPROCEEDINGS(new String[]{"author", "title", "booktitle", "year", // <- mandatory fields
+      "editor", "volumeNumber", "series", "pages", "address",
+      "month", "organization", "publisher", "note", "key"}),
+   BOOK(new String[]{"author", "title", "publisher", "year", // <- mandatory fields
+      "volume", "series", "address", "edition", "month", "note", "key"}),
+   ARTICLE(new String[]{"author", "title", "journal", "year", // <- mandatory fields
+      "volume", "number", "pages", "month", "note", "key"});
+      
+   private final String[] applicableFields;
+
+   private CitationType(String[] applicableFields) {
+      this.applicableFields = applicableFields;
+   }
 
    public String[] getApplicableFields() {
       return applicableFields;
    }
-    
 }
